736aece2f86762f0ccada412f1afe817c9407271
= <=>
2022-09-04 13:38:21 +0300

    last version

--- routes/todos.js
+++ routes/todos.js
@@ -19,19 +19,19 @@ router.get("/:id", (req, res, next) => {
 
 })
 
-router.post("/", (res, req, next) => {
-    const { body } = req;
-    
-    if (typeof body.name !== 'string') {
-        return next(createError(422, "Validation Error"))
-    }
+router.post("/", (req, res, next) => {
 
+    const { body } = req;
+    if (typeof body.name !== 'string') {
+        return next(createError(422, 'Validation Error'))
+    }
     const newTodo = {
         id: todos.length + 1,
         name: body.name,
         completed: false
     }
     todos.push(newTodo)
+
     res.status(201).json(newTodo)
 })
 module.exports = router;
